ed03e8df062639c860ee9b20fd30e315368af40b
Pan Ktos <lambda19935@gmail.com>
2020-09-05 08:22:33 +0200

    Dodaj pliki projektów.

---
+++ Z13-SAAF/Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z13_SAAF
+{
+    public class Context : DbContext
+    {
+        public DbSet<Model> Models { get; set; }
+        public Context()
+        {
+
+            Models.Add(new Model() { Amount = 5, Text = "test" });
+
+        }
+    }
+}
---
+++ Z13-SAAF/DBDataProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z13_SAAF
+{
+    public class DBDataProvider : IDataProvider
+    {
+        public DBDataProvider(Context context)
+        {
+            Context = context;
+        }
+        public Context Context { get; private set; }
+
+        public int GetData()
+        {
+            return 1;
+            //return Context.Models.FirstOrDefault().Amount;
+        }
+    }
+}
